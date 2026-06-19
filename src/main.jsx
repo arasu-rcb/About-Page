4e2a4efcfa812bf7ae2457355633958c3853dc77
arasu-murali <arasu.king.014@gmail.com>
2026-06-19 19:34:47 +0530

    UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Initialize theme based on user preference or OS setting
-if (
-  localStorage.theme === 'dark' ||
-  (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
